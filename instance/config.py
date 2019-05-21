cb7d6ae595791f3bb81c15a5d576f90e515d12f7
Hilton Pintor <hiltonpintor@gmail.com>
2019-05-21 10:15:34 -0300

    Create The App

--- instance/config.py
+++ instance/config.py
@@ -1,1 +1,39 @@
-import OS
+import os
+
+class Config(object):
+	"""Parent configuration class."""
+	DEBUG = False
+	CSRF_ENABLED = True
+	SECRET = os.getenv('SECRET')
+	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+
+class DevelopmentConfig(Config):
+	"""Configuration for Development."""
+	DEBUG = True
+
+
+class TestingConfig(Config):
+	"""Configuration for Testing, with separate database."""
+	TESTING = True
+	DEBUG = True
+	SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+
+
+class StagingConfig(Config):
+	"""Configuration for staging."""
+	DEBUG = True
+
+
+class ProductionConfig(Config):
+	"""Configuration for Production."""
+	DEBUG = False
+	TESTING = False
+
+
+app_config = {
+	'development': DevelopmentConfig,
+	'testing': TestingConfig,
+	'staging': StagingConfig,
+	'production': ProductionConfig,
+}
