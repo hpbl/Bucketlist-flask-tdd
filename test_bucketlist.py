0fe73aef0c76c279d2823433bb708e8457e2b687
Hilton Pintor <hiltonpintor@gmail.com>
2019-05-21 11:45:39 -0300

    Time To Test!

---
+++ test_bucketlist.py
@@ -0,0 +1,89 @@
+import unittest
+import os
+import json
+from app import create_app, db
+
+
+class BucketlistTestCase(unittest.TestCase):
+	"""This class represents the bucketlist test case"""
+
+	def setUp(self):
+		"""Define test variables and initialize app."""
+		self.app = create_app(config_name="testing")
+		self.client = self.app.test_client
+		self.bucketlist = { 'name': 'Go to Alagoinhas for vacation' }
+
+		# binds the app to the current context
+		with self.app.app_context():
+			# create all tables
+			db.create_all()
+
+
+	def test_bucketlist_creation(self):
+		"""Test API can create a bucketlist (POST request)."""
+		res = self.client().post('/bucketlists/', data=self.bucketlist)
+		self.assertEqual(res.status_code, 201)
+		self.assertIn('Go to Alagoinhas', str(res.data))
+
+
+	def test_api_can_get_all_bucketlists(self):
+		"""Test API can get all bucketlists (GET request)."""
+		res = self.client().post('/bucketlists/', data=self.bucketlist)
+		self.assertEqual(res.status_code, 201)
+
+		res = self.client().get('/bucketlists/')
+		self.assertEqual(res.status_code, 200)
+		self.assertIn('Go to Alagoinhas', str(res.data))
+
+
+	def test_api_can_get_bucketlist_by_id(self):
+		"""Test API can get a single bucketlist by using it's id."""
+		postRes = self.client().post('/bucketlist/', data=self.bucketlist)
+		self.assertEqual(postRes.status_code, 201)
+		insertedID = json.loads(postRes.data.devode('utf-8').replace("'", "\""))
+
+		res = self.client().get('/bucketlists/{}'.format(insertedID))
+		self.assertEqual(res.status_code, 200)
+		self.assertIn('Go to Alagoinhas', str(result.data))
+
+
+	def test_bucketlist_can_be_edited(self):
+		"""Test API can edit an existing bucketlist. (PUT request)"""
+		rv = self.client().post('/bucketlists/', data={ 'name': 'P = NP' })
+		self.assertEqual(rv.status_code, 201)
+
+		res = self.client().put(
+			'/bucketlists/1',
+			data={ 'name': 'P != NP' }
+		)
+		self.assertEqual(res.status_code, 200)
+
+		result = self.client().get('/bucketlist/1')
+		self.assertIn('!=', str(results.data))
+
+
+	def test_bucketlist_deletion(self):
+		"""Test API can delete an existing bucketlist. (DELETE request)"""
+		rv = self.client().post('/bucketlists/', data={ 'name': 'P = NP' })
+		self.assertEqual(rv.status_code, 201)
+
+		res = self.client().delete('/bucketlists/1')
+		self.assertEqual(res.status_code, 200)
+
+		result = self.client().get('/bucketlist/1')
+		self.assertEqual(result.status_code, 404)
+
+
+	def tearDown(self):
+		"""teardown all initialized variables"""
+		with self.app.app_context():
+			# drop all tables
+			db.session.remove()
+			db.drop_all()
+
+
+# Make the tests executable
+if __name__ == "__main__":
+	unittest.main()
+
+
