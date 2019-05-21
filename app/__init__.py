6d5aa405376e2980b8087e3ca05a934e9198963d
Hilton Pintor <hiltonpintor@gmail.com>
2019-05-21 11:56:09 -0300

    API Functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from flask import request, jsonify, abort
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,5 +14,40 @@ def create_app(config_name):
 	app.config.from_pyfile('config.py')
 	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 	db.init_app(app)
+
+	# avoiding circular imports
+	from app.models import Bucketlist
+
+	@app.route('/bucketlists/', methods=['POST', 'GET'])
+	def bucketlists():
+		if request.method == "POST":
+			name = str(request.data.get('name', ''))
+			if name:
+				bucketlist = Bucketlist(name=name)
+				bucketlist.save()
+				response = jsonify({
+					'id': bucketlist.id,
+					'name': bucketlist.name,
+					'date_created': bucketlist.date_created,
+					'date_modified': bucketlist.date_modified	
+				})
+				response.status_code = 201
+				return response
+		else:
+			bucketlists = Bucketlist.get_all()
+			results = []
+
+			for bucketlist in bucketlists:
+				obj = {
+					'id': bucketlist.id,
+					'name': bucketlist.name,
+					'date_created': bucketlist.date_created,
+					'date_modified': bucketlist.date_modified
+				}
+				results.append(obj)
+
+			response = jsonify(results)
+			response.status_code = 200
+			return response
 
 	return app
